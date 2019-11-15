779854fa012829a4ca478f54a39f0c3b537c9e65
ham952 <ham952@hotmail.com>
2019-11-16 00:40:29 +0500

    Update epochcheckpoint.py

--- pyimagesearch/callbacks/epochcheckpoint.py
+++ pyimagesearch/callbacks/epochcheckpoint.py
@@ -3,7 +3,7 @@ from keras.callbacks import Callback
 import os
 
 class EpochCheckpoint(Callback):
-	def __init__(self, outputPath, every=5, startAt=0):
+	def __init__(self, outputPath, every, startAt=0):
 		# call the parent constructor
 		super(Callback, self).__init__()
 
@@ -18,7 +18,7 @@ class EpochCheckpoint(Callback):
 		# check to see if the model should be serialized to disk
 		if (self.intEpoch + 1) % self.every == 0:
 			p = os.path.sep.join([self.outputPath,
-				"epoch_{}.hdf5".format(self.intEpoch + 1)])
+				"epoch_{}.h5".format(self.intEpoch + 1)])
 			self.model.save(p, overwrite=True)
 
 		# increment the internal epoch counter
